e22b0acf86784ea6800588475c3a32bc8d986328
HenryWauzivuff <145900606+HEofOttoman@users.noreply.github.com>
2026-07-28 22:39:06 +1000

    Okay, this kinda works now!
    Did things that supposedly fixed the issues, after Ed's testing. Not sure if the thing actually works.
    Changed openmeteo url and the printed format string

--- src/main.cpp
+++ src/main.cpp
@@ -14,51 +14,60 @@ const int   daylightOffset_sec = 0;
 const char* ntpServer = "au.pool.ntp.org";
 
 // const char* openmeteo_url = "https://open-meteo.com/en/docs?hourly=temperature_2m,showers,snowfall,snow_depth,rain,precipitation,weather_code,uv_index&timezone=auto&latitude=40.7143,-35.2835,-6.2146,-1.2833&longitude=-74.006,149.1281,106.8451,36.8167&forecast_days=1&bounding_box=-90,-180,90,180&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,precipitation_probability_max,temperature_2m_mean,weather_code&current=temperature_2m,precipitation,rain,showers,snowfall,is_day,apparent_temperature,weather_code,cloud_cover&timeformat=unixtime#current_weather";
-const char* openmeteo_url = "https://open-meteo.com/en/docs?hourly=temperature_2m,showers,snowfall,snow_depth,rain,precipitation,weather_code,uv_index&timezone=auto&latitude=40.7143,-35.2835,-6.2146,-1.2833&longitude=-74.006,149.1281,106.8451,36.8167&forecast_days=1&bounding_box=-90,-180,90,180&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,rain_sum,precipitation_probability_max,temperature_2m_mean,weather_code&current=temperature_2m,precipitation,rain,showers,snowfall,is_day,apparent_temperature,weather_code,cloud_cover&timeformat=unixtime#current_weather";
+const char* openmeteo_url = "https://api.open-meteo.com/v1/forecast?latitude=-35.2835&longitude=149.1281&daily=temperature_2m_max,temperature_2m_min,precipitation_probability_max&current=temperature_2m,is_day&timezone=Australia%2FSydney";
+float latitude = 35.2835;
+float longitude = 149.1281;
 
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#include <U8g2lib.h>
-#include <U8x8lib.h>
+// #include <U8g2lib.h>
+// #include <U8x8lib.h>
 // U8G2_SSD1306_128X64_NONAME_1_HW_I2C display(U8G2_R0, U8X8_PIN_NONE);
-U8X8_SH1106_128X64_NONAME_HW_I2C display(U8X8_PIN_NONE, U8X8_PIN_NONE);
-// Adafruit_SSD1306 display(128, 64, &Wire, -1);
+// U8X8_SH1106_128X64_NONAME_HW_I2C display(U8X8_PIN_NONE, U8X8_PIN_NONE);
+Adafruit_SSD1306 display(128, 64, &Wire, -1);
+
+const uint8_t buzzerPin = 2;
 
 void setup() {
     // Serial.begin(115200);
     Serial.begin(115200);
 
-    // Wire.begin(); // Maybe this can work?
+    Wire.begin(); // Maybe this can work?
 
-    // display.begin(SSD1306_SWITCHCAPVCC, 0x3D);
-    // if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
-    //     Serial.println("OLED failed to boot.");
-    // }
-    // Serial.println("OLED Initialised!");
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println("OLED failed to boot.");
+    }
+    Serial.println("OLED Initialised!");
 
-    // display.clearDisplay();
-    // display.print("Display reset.");
-    // display.setTextColor(WHITE);
-    // display.setTextSize(1);
-    // Serial.print("Over I2C!");
+    display.clearDisplay();
+    display.display();
+    display.print("Display reset.");
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.clearDisplay();
+    display.setCursor(0,0);
+    delay(100);
+    display.print("Hello?");
+    Serial.print("Over I2C!");
     
-    display.begin();
-    
-    display.setFont(u8g2_font_ncenB08_tr);
-    display.drawString(0, 15, "Mode: SH1106");
-    display.drawString(0, 30, "Check border alignment:"); // This is for U8G2 SH1106
+    // display.begin();
+    // display.setFont(u8g2_font_ncenB08_tr);
+    // display.drawString(0, 15, "Mode: SH1106");
+    // display.drawString(0, 30, "Check border alignment:"); // This is for U8G2 SH1106
     // display.drawStr(0, 15, "Mode: SH1106");
     // display.drawStr(0, 30, "Check border alignment:"); // This is for U8G2 SSD1306
     // Draw a box along the absolute edges (0,0 to 127,63)
     // display.drawFrame(0, 0, 128, 64);
 
-    Serial.println("OLED SH1106 Initialised!");
+    // Serial.println("OLED SH1106 Initialised!");
 
     // Uncomment when cardputer porting 
     // M5Cardputer.begin();
 
+    pinMode(buzzerPin, OUTPUT);
+    tone(buzzerPin, 100, 100);
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(SECRET_SSID, SECRET_PASS);
@@ -98,12 +107,15 @@ void fetchHTTP() {
             return;
         }
 
-        const char* city = obj["city"];
-        float temp = (double)obj["main"]["temperature"];
-        int weather = obj["main"]["current_weather"];
+        // const char* city = obj[""];
+        float temp = (double)obj["current"]["temperature"];
+        // float rainChance = (doubleobj["current"]["precipitation_probability_max"];
+        float minTemp = (double)obj["current"]["temperature_2m_min"]; 
+        float maxTemp = (double)obj["current"]["temperature_2m_max"];
 
         // Serial.println((double)obj["current_weather"]["temperature"]);
-        Serial.printf("Weather in %s: %.1f°C", city, temp);
+        Serial.printf("Weather for %.4f lat %.4f long, for today : \n", latitude, longitude);
+        Serial.printf("%.1f°C, with min of %.1f°C and max of %.1f°C \n", temp, minTemp, maxTemp);
 
     } else {
         Serial.print("ERROR SENDING REQUEST");
